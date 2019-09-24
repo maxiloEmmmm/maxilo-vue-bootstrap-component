7147703d2d8ac576a11783884f663da628fc17cf
2802146520@qq.com <2802146520@qq.com>
2019-09-24 22:24:44 +0800

    last

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 
 module.exports = {
   presets: [
-    ["@babel/preset-env",{"useBuiltIns": "usage"}]
+    ["@babel/preset-env", {
+      "useBuiltIns": "usage",
+      "corejs": {
+        "version": 3,
+        "proposals": true
+      }}]
   ],
   plugins: [["@babel/plugin-transform-runtime", {
     "regenerator": false
